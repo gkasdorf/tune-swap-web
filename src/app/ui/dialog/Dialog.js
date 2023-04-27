24ff08170bac9190723c07cccbaba88f5831ec5c
Gavin Kasdorf <me@gkasdorf.com>
2023-04-26 16:54:47 -0800

    adjustments to dialog

--- src/app/ui/dialog/Dialog.js
+++ src/app/ui/dialog/Dialog.js
@@ -1,11 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Dialog = ({max = "max-w-2xl", children}) => {
+const Dialog = ({max = "mx-auto", children}) => {
     return (
         <div className={"flex flex-col items-center"}>
             <div className={`w-full ${max}`}>
-                <div className={"bg-white shadow-md rounded px-4 md:px-8 pt-6 pb-8 mb-4"}>
+                <div className={"bg-white shadow-md rounded-lg px-4 md:px-8 pt-6 pb-8 mb-4"}>
                     {children}
                 </div>
             </div>
